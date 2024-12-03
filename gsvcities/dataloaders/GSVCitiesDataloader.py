dc3a8b70269fc839f0cca77b5ac05660be7d0df2
Usman Maqbool <usmanmaqbool@outlook.com>
2024-12-03 08:19:17 -0500

    mixvpr-fusion-plus-debug-no-GAT

--- gsvcities/dataloaders/GSVCitiesDataloader.py
+++ gsvcities/dataloaders/GSVCitiesDataloader.py
@@ -14,6 +14,8 @@ from prettytable import PrettyTable
 IMAGENET_MEAN_STD = {'mean': [0.485, 0.456, 0.406], 
                      'std': [0.229, 0.224, 0.225]}
 
+
+
 VIT_MEAN_STD = {'mean': [0.5, 0.5, 0.5], 
                 'std': [0.5, 0.5, 0.5]}
 
